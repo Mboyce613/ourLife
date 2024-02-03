50d769d7117e7cd8e42d1ba5c3d94309c3372f41
michael <michael.a.boyce999@gmail.com>
2024-02-02 19:00:30 -0800

    should fix fk

--- app/models/user_family.py
+++ app/models/user_family.py
@@ -1,11 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import Column, ForeignKey, Table
-from .db import db
+from .db import db, add_prefix_for_prod
 
 Base = declarative_base()
 
 user_families = Table(
     "user_families",
     db.Model.metadata,
-    Column("user_id", ForeignKey("users.id"), primary_key=True),
-    Column("family_id", ForeignKey("families.id"), primary_key=True))
+    Column("user_id", ForeignKey(add_prefix_for_prod("users.id")), primary_key=True),
+    Column("family_id", ForeignKey(add_prefix_for_prod("families.id")), primary_key=True))
