3eb209718dab0a8ea3e1169bbab9e7cce08b8f6b
michael <michael.a.boyce999@gmail.com>
2024-02-05 19:47:54 -0800

    starting components

--- react-vite/src/components/Home/Home.jsx
+++ react-vite/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserById } from "../../redux/user";
+import './Home.css'
 
 const Home = () => {
     const sessionUser = useSelector((state) => state.session.user);
+    console.log(sessionUser.appointments)
     // const dispatch = useDispatch()
     // const [isLoaded, setIsLoaded] = useState(false)
 
@@ -17,6 +19,13 @@ const Home = () => {
     return (
         <>
         <h1>Hello {sessionUser.first_name}</h1>
+        <section className="homepage">
+        {/* <p>{sessionUser.appointments[1]}</p> */}
+        <p>Placeholder Families</p>
+        <p>Placeholder Calendar</p>
+        <p>Placeholder Budget</p>
+        <p>Placeholder Shopping</p>
+        </section>
         </>
     )
 }
