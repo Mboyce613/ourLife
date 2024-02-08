7ff09d908514c5f5cdc037b69276df1a46995d71
michael <michael.a.boyce999@gmail.com>
2024-02-07 21:46:17 -0800

    normalized

--- react-vite/src/redux/user.js
+++ react-vite/src/redux/user.js
@@ -20,13 +20,60 @@ const userReducer = (state = {}, action)=>{
     let newState = null
     switch(action.type){
         case LOAD_USER:
-            newState = {...state, user:{}}
+            newState = {...state}
             // console.log("ACTION", action, 'line 24')
             // console.log(action.avatar, '-----store')
             if(action.user && action.user !== undefined){
-                // console.log("LINE 27", action.user)
+                console.log("LINE 27", action.user)
                 action.user.forEach(ele => {
-                    newState = ele
+                    newState['id'] = ele.id
+                    newState['first_name'] = ele.first_name
+                    newState['last_name'] = ele.last_name
+                    newState['email'] = ele.email
+                    newState['is_dependent'] = ele.is_dependent
+                    if(ele.appointments){
+                        newState['appointments'] = {}
+                        ele.appointments.forEach(app => {
+                            newState['appointments'][app.id] = app
+                        });
+                      }
+                    if(ele.expenses){
+                        newState['expenses'] = {}
+                        ele.expenses.forEach(app => {
+                            newState['expenses'][app.id] = app
+                        });
+                      }
+                    if(ele.incomes){
+                        newState['incomes'] = {}
+                        ele.incomes.forEach(app => {
+                            newState['incomes'][app.id] = app
+                        });
+                      }
+                    if(ele.medications){
+                        newState['medications'] = {}
+                        ele.medications.forEach(app => {
+                            newState['medications'][app.id] = app
+                        });
+                      }
+                    // if(ele.families){
+                    //     newState['families'] = {}
+                    //     ele.families.forEach(app => {
+                    //         newState['families'][app.id] = app
+                    //     });
+                    //   }
+                    if(ele.families){
+                        newState['families'] = {}
+                        ele.families.forEach(family => {
+                          const newList = {}
+                          // console.log("SHOPP", family.shopping_lists)
+                          family.shopping_lists.forEach(list=>{
+                            newList[list.id] = list
+                          })
+                          // console.log("NEWSHOPP", newList)
+                          family.shopping_lists = newList
+                          newState['families'][family.id] = family
+                        });
+                      }
                 })
             }else{
                 newState = null
